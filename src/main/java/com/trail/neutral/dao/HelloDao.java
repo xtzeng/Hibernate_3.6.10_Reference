51d8a753f0f6841e2dcd3023276c3543cb00b8c6
xtzeng <xiaoti_zeng@126.com>
2014-07-09 17:32:54 +0800

    add

--- src/main/java/com/trail/neutral/dao/HelloDao.java
+++ src/main/java/com/trail/neutral/dao/HelloDao.java
@@ -1,15 +1,31 @@
 package com.trail.neutral.dao;
 
+
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class HelloDao {
+import com.trail.neutral.pojos.Cat;
 
+@Repository("helloDao")
+public class HelloDao extends HibernateDaoSupport{
+
+	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
+	
+	
 	public void save() {
+		
+		
+		Session session = this.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Cat cat = new Cat();
+		cat.setName("喵喵");
+		session.save(cat);
+		session.getTransaction().commit();
 		logger.info("save...");
 	}
 }
