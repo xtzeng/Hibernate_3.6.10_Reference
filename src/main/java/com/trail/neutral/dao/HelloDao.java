3b3e9daf189ec77fc2a4b6de19eebd39b7fad30b
xtzeng <xiaoti_zeng@126.com>
2014-07-09 22:31:06 +0800

    add hibernateTemplate

--- src/main/java/com/trail/neutral/dao/HelloDao.java
+++ src/main/java/com/trail/neutral/dao/HelloDao.java
@@ -1,31 +1,30 @@
 package com.trail.neutral.dao;
 
 
-import org.hibernate.Session;
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.trail.neutral.pojos.Cat;
 
 @Repository("helloDao")
-public class HelloDao extends HibernateDaoSupport{
+public class HelloDao {
 
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	
+	@Resource(name="hibernateTemplate")
+	private HibernateTemplate hibernateTemplate;
 	
 	public void save() {
 		
 		
-		Session session = this.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
 		Cat cat = new Cat();
 		cat.setName("喵喵");
-		session.save(cat);
-		session.getTransaction().commit();
+		this.hibernateTemplate.save(cat);
 		logger.info("save...");
 	}
 }
